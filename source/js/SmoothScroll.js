846c60ef9bfbe4a2045d86e15c5b25e77e963b0f
une666 <vachehu@qq.com>
2019-12-25 14:41:09 +0800

    update index

--- source/js/SmoothScroll.js
+++ source/js/SmoothScroll.js
@@ -292,7 +292,12 @@ function wheel(event) {
     }
     
     scrollArray(overflowing, -deltaX, -deltaY);
-    event.preventDefault();
+    if (event.cancelable) {
+        // 判断默认行为是否已经被禁用
+        if (!event.defaultPrevented) {
+            event.preventDefault();
+        }
+    }
 }
 
 /**
